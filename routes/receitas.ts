9ff7b291f499b703f9dc827efe5e419dedbb56d5
André Henrique <andre.alves68@outlook.com>
2024-12-03 07:58:25 -0300

    merge

--- routes/receitas.ts
+++ routes/receitas.ts
@@ -2,11 +2,15 @@ import app = require("teem");
 
 class ReceitasRoute {
 	public async index(req: app.Request, res: app.Response) {
-		res.render("receitas/receita");
+		res.render("receitas/index");
 	}
 
 	public async criar(req: app.Request, res: app.Response) {
-		res.render("receitas/criar_receita");
+		res.render("receitas/criar");
+	}
+
+	public async receita(req: app.Request, res: app.Response) {
+		res.render("receitas/index");
 	}
 
 }
